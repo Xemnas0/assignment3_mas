b8350a51daea181ef0ad9c950dd2d4fbf4303b0a
Tobias <testreen@kth.se>
2019-04-09 15:18:11 +0200

    Added random AI

--- a3c_bipedalWalker.py
+++ a3c_bipedalWalker.py
@@ -5,16 +5,18 @@ import numpy as np
 from queue import Queue
 import argparse
 import matplotlib.pyplot as plt
+import Box2D
+from Box2D.b2 import (edgeShape, circleShape, fixtureDef, polygonShape, revoluteJointDef, contactListener)
 
 import tensorflow as tf
 from tensorflow.python import keras
 from tensorflow.python.keras import layers
 
-tf.enable_eager_execution()
 
-parser = argparse.ArgumentParser(description='Run A3C algorithm on the game '
-											 'Bipedal Walker.')
-parser.add_argument('--algorithm', default='a3c', type=str,
+parser = argparse.ArgumentParser(description='Run A3C algorithm on an OpenAI gym game.')
+parser.add_argument('--env_name', default='BipedalWalker-v2', type=str,
+					help='Choose environment (default=\'BipedalWalker-v2\'.')
+parser.add_argument('--algorithm', default='random', type=str,
 					help='Choose between \'a3c\' and \'random\'.')
 parser.add_argument('--train', dest='train', action='store_true',
 					help='Train our model.')
@@ -30,6 +32,38 @@ parser.add_argument('--save-dir', default='/tmp/', type=str,
 					help='Directory in which you desire to save the model.')
 args = parser.parse_args()
 
+
+def record(episode,
+           episode_reward,
+           worker_idx,
+           global_ep_reward,
+           result_queue,
+           total_loss,
+           num_steps):
+  """Helper function to store score and print statistics.
+  Arguments:
+    episode: Current episode
+    episode_reward: Reward accumulated over the current episode
+    worker_idx: Which thread (worker)
+    global_ep_reward: The moving average of the global reward
+    result_queue: Queue storing the moving average of the scores
+    total_loss: The total loss accumualted over the current episode
+    num_steps: The number of steps the episode took to complete
+  """
+  if global_ep_reward == 0:
+    global_ep_reward = episode_reward
+  else:
+    global_ep_reward = global_ep_reward * 0.99 + episode_reward * 0.01
+  print(
+      f"Episode: {episode} | "
+      f"Moving Average Reward: {int(global_ep_reward)} | "
+      f"Episode Reward: {int(episode_reward)} | "
+      f"Loss: {int(total_loss / float(num_steps) * 1000) / 1000} | "
+      f"Steps: {num_steps} | "
+      f"Worker: {worker_idx}"
+  )
+  result_queue.put(global_ep_reward)
+  return global_ep_reward
 
 class RandomAgent:
 	"""Random Agent that will play the specified game
@@ -66,3 +100,11 @@ class RandomAgent:
 		final_avg = reward_avg / float(self.max_episodes)
 		print("Average score across {} episodes: {}".format(self.max_episodes, final_avg))
 		return final_avg
+
+if __name__ == '__main__':
+  print(args)
+  random = RandomAgent(args.env_name, args.max_eps)
+  if args.train:
+    random.train()
+  else:
+    random.run()
